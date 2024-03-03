771adad637e4f187fd7ad61a7bf338f9a4706ad1
Jose K Shaju <111490066+JoseS1510@users.noreply.github.com>
2024-03-03 11:38:35 +0530

    Add files via upload

---
+++ P18.php
@@ -0,0 +1,16 @@
+<?php
+
+$cookie_name = "user";
+$cookie_value = "John Doe";
+
+$cookie_expiry = time() + (86400 * 1); 
+
+setcookie($cookie_name, $cookie_value, $cookie_expiry, "/"); 
+
+if(isset($_COOKIE[$cookie_name])) {
+    echo "Cookie name '" . $cookie_name . "' is set!<br>";
+    echo "Value is: " . $_COOKIE[$cookie_name];
+} else {
+    echo "Cookie named '" . $cookie_name . "' is not set!";
+}
+?>
